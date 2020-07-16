23763df6fc1e6572f4baecc8e5016d453cc62e07
Luqman Hakim <luqeckr@gmail.com>
2020-07-17 06:20:56 +0800

    dynamic menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Menu {
+  title: string
+  link: string
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,6 +12,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  menus: Menu[] = [
+    { title: 'Beranda', link: '/' },
+    { title: 'Menu1', link: '/menu1' },
+    { title: 'Menu2', link: '/menu2' },
+    { title: 'Menu3', link: '/menu3' },
+  ]
   constructor() { }
 
   ngOnInit(): void {
