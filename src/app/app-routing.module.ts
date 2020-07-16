413f1e988d1c706e58e6c21c17d74b7e690aa1e6
Luqman Hakim <luqeckr@gmail.com>
2020-07-16 23:28:06 +0800

    komponen with routing, lazy-load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BerandaComponent } from './beranda/beranda.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BerandaComponent },
+  { path: 'menu1', loadChildren: () => import('./menu1/menu1.module').then(m => m.Menu1Module) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
