975c257e04643a7b3740dfd94619f85b10e81d58
Luqman Hakim <luqeckr@gmail.com>
2020-07-16 22:06:02 +0800

    komponen

---
+++ src/app/beranda/beranda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beranda',
+  templateUrl: './beranda.component.html',
+  styleUrls: ['./beranda.component.less']
+})
+export class BerandaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
